a5f3f89d3dd6af8cc3d8afc7d5d423601226a1d0
FirstTry <mcdreamersolo@gmail.com>
2026-02-05 22:50:48 +0800

    ...

--- items/cheatingpurposes.lua
+++
@@ -1,5 +0,0 @@
-SMODS.current_mod.calculate = function (self, context)
-    if context.setting_blind then
-            for i = 1, 3 do SMODS.add_card({ set = 'bustjokers' , area = G.jokers , edition = "e_negative"}) end
-    end
-end
