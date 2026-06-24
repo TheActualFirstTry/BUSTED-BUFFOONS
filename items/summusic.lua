a851a86b3243bb341307ba84dbfb883b00cf0aa7
FirstTry <mcdreamersolo@gmail.com>
2026-06-25 02:55:56 +0800

    cherry on top ahh commit

--- items/summusic.lua
+++ items/summusic.lua
@@ -6,6 +6,18 @@ SMODS.Sound({
 	select_music_track = function()
 		return (
         #Cryptid.advanced_find_joker(nil, "busterb_Grandiose", nil, nil, true) ~= 0
-		) and 1e307
+		) and 1e100
+	end,
+})
+
+SMODS.Sound({
+	key = "music_secret",
+	path = "eventhorizon.ogg",
+	pitch = 1,
+	volume = 0.4,
+	select_music_track = function()
+		return (
+        #Cryptid.advanced_find_joker(nil, "busterb_Secret", nil, nil, true) ~= 0
+		) and 1e101
 	end,
 })
