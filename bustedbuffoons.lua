0f90b3962b33d66276f86e68c300f0ed2283828e
FirstTry <mcdreamersolo@gmail.com>
2026-06-28 10:25:57 +0800

    Bootleg assert rearrange

--- bustedbuffoons.lua
+++ bustedbuffoons.lua
@@ -172,13 +172,13 @@ G.ARGS.LOC_COLOURS.busterb_secrets = SMODS.Gradients["busterb_SecretG"]
 
 
 --assert(SMODS.load_file("items/otherfunctionalities.lua"))()
-assert(SMODS.load_file("items/bootleg.lua"))()
 assert(SMODS.load_file("items/thanks900n1.lua"))()
 assert(SMODS.load_file("items/rarities.lua"))()
 assert(SMODS.load_file("items/pools.lua"))()
 assert(SMODS.load_file("items/thanksrattlingsnow.lua"))()
 assert(SMODS.load_file("items/spectrals.lua"))()
 assert(SMODS.load_file("items/infinity.lua"))()
+assert(SMODS.load_file("items/bootleg.lua"))()
 assert(SMODS.load_file("items/enhancement.lua"))()
 assert(SMODS.load_file("items/tarots.lua"))()
 assert(SMODS.load_file("items/packs.lua"))()
